ca321d649e5981e95492c7b1015c23c045b95846
kasusa <kasusaland@gmail.com>
2020-01-20 15:22:38 +0800

    readme update

--- winQRcode_ch/Form1.Designer.cs
+++ winQRcode_ch/Form1.Designer.cs
@@ -64,9 +64,10 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 888);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 772);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1669, 37);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1530, 37);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -81,10 +82,10 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox1.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(3, 31);
+            this.textBox1.Location = new System.Drawing.Point(3, 27);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(905, 487);
+            this.textBox1.Size = new System.Drawing.Size(829, 442);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -92,9 +93,9 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(745, 110);
+            this.button1.Location = new System.Drawing.Point(683, 96);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 131);
+            this.button1.Size = new System.Drawing.Size(120, 115);
             this.button1.TabIndex = 4;
             this.button1.Text = "生成 QRcode";
             this.button1.UseVisualStyleBackColor = true;
@@ -110,14 +111,14 @@
             this.帮助ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1669, 42);
+            this.menuStrip1.Size = new System.Drawing.Size(1530, 36);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // 保存图片ToolStripMenuItem
             // 
             this.保存图片ToolStripMenuItem.Name = "保存图片ToolStripMenuItem";
-            this.保存图片ToolStripMenuItem.Size = new System.Drawing.Size(135, 38);
+            this.保存图片ToolStripMenuItem.Size = new System.Drawing.Size(135, 32);
             this.保存图片ToolStripMenuItem.Text = "保存为图片";
             this.保存图片ToolStripMenuItem.Click += new System.EventHandler(this.保存图片ToolStripMenuItem_Click);
             // 
@@ -125,24 +126,24 @@
             // 
             this.打开保存位置ToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
             this.打开保存位置ToolStripMenuItem.Name = "打开保存位置ToolStripMenuItem";
-            this.打开保存位置ToolStripMenuItem.Size = new System.Drawing.Size(156, 38);
+            this.打开保存位置ToolStripMenuItem.Size = new System.Drawing.Size(156, 32);
             this.打开保存位置ToolStripMenuItem.Text = "打开保存位置";
             this.打开保存位置ToolStripMenuItem.Click += new System.EventHandler(this.打开保存位置ToolStripMenuItem_Click);
             // 
             // 帮助ToolStripMenuItem
             // 
             this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
-            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(72, 38);
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(72, 32);
             this.帮助ToolStripMenuItem.Text = "关于";
             this.帮助ToolStripMenuItem.Click += new System.EventHandler(this.帮助ToolStripMenuItem_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Location = new System.Drawing.Point(19, 19);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.Size = new System.Drawing.Size(106, 21);
             this.label1.TabIndex = 7;
             this.label1.Text = "清晰度:10";
             // 
@@ -150,9 +151,9 @@
             // 
             this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 196);
+            this.checkBox1.Location = new System.Drawing.Point(17, 172);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(138, 29);
+            this.checkBox1.Size = new System.Drawing.Size(120, 25);
             this.checkBox1.TabIndex = 8;
             this.checkBox1.Text = "同步生成";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -171,19 +172,19 @@
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(739, 599);
+            this.panel1.Location = new System.Drawing.Point(677, 524);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(911, 256);
+            this.panel1.Size = new System.Drawing.Size(835, 224);
             this.panel1.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label2.Location = new System.Drawing.Point(21, 78);
+            this.label2.Location = new System.Drawing.Point(19, 68);
             this.label2.Name = "label2";
             this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label2.Size = new System.Drawing.Size(563, 25);
+            this.label2.Size = new System.Drawing.Size(473, 21);
             this.label2.TabIndex = 13;
             this.label2.Text = "默认清晰度是10，越大越清晰，但是生成速度越慢";
             // 
@@ -191,9 +192,9 @@
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(673, 110);
+            this.button2.Location = new System.Drawing.Point(617, 96);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(60, 131);
+            this.button2.Size = new System.Drawing.Size(55, 115);
             this.button2.TabIndex = 12;
             this.button2.Text = "清空历史";
             this.button2.UseVisualStyleBackColor = true;
@@ -203,9 +204,9 @@
             // 
             this.checkBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(163, 196);
+            this.checkBox3.Location = new System.Drawing.Point(149, 172);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(155, 29);
+            this.checkBox3.Size = new System.Drawing.Size(135, 25);
             this.checkBox3.TabIndex = 11;
             this.checkBox3.Text = "Game mode";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -217,9 +218,9 @@
             this.checkBox2.AutoSize = true;
             this.checkBox2.Checked = true;
             this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox2.Location = new System.Drawing.Point(328, 196);
+            this.checkBox2.Location = new System.Drawing.Point(301, 172);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(138, 29);
+            this.checkBox2.Size = new System.Drawing.Size(120, 25);
             this.checkBox2.TabIndex = 10;
             this.checkBox2.Text = "自动清空";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -227,11 +228,11 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(186, 12);
+            this.trackBar1.Location = new System.Drawing.Point(171, 10);
             this.trackBar1.Maximum = 30;
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(688, 80);
+            this.trackBar1.Size = new System.Drawing.Size(631, 80);
             this.trackBar1.TabIndex = 9;
             this.trackBar1.Value = 10;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
@@ -244,18 +245,18 @@
             this.listBox1.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 24;
-            this.listBox1.Location = new System.Drawing.Point(3, 31);
+            this.listBox1.Location = new System.Drawing.Point(3, 27);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(685, 222);
+            this.listBox1.Size = new System.Drawing.Size(627, 194);
             this.listBox1.TabIndex = 11;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
             // 
             // progressBar1
             // 
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(1411, 896);
+            this.progressBar1.Location = new System.Drawing.Point(1293, 784);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(225, 17);
+            this.progressBar1.Size = new System.Drawing.Size(206, 15);
             this.progressBar1.TabIndex = 12;
             // 
             // pictureBox1
@@ -264,9 +265,9 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Image = global::winQRcode_ch.Properties.Resources.qr_demo_2x;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox1.Location = new System.Drawing.Point(11, 48);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(705, 521);
+            this.pictureBox1.Size = new System.Drawing.Size(646, 456);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -277,9 +278,9 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(26, 599);
+            this.groupBox1.Location = new System.Drawing.Point(24, 524);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(691, 256);
+            this.groupBox1.Size = new System.Drawing.Size(633, 224);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "历史记录";
@@ -290,9 +291,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Location = new System.Drawing.Point(739, 55);
+            this.groupBox2.Location = new System.Drawing.Point(677, 32);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(911, 521);
+            this.groupBox2.Size = new System.Drawing.Size(835, 472);
             this.groupBox2.TabIndex = 14;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "输入区";
@@ -300,9 +301,9 @@
             // Form1
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1669, 925);
+            this.ClientSize = new System.Drawing.Size(1530, 809);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.progressBar1);
